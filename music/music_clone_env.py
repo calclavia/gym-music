b73c8db8e32c41ee61764218a573b7925d9d138a
Henry Mao <admin@calclavia.com>
2017-01-21 20:37:58 -0800

    More work on clone

--- music/music_clone_env.py
+++ music/music_clone_env.py
@@ -1,33 +1,38 @@
 import numpy as np
 import random
 from .music_env import MusicEnv
+from .util import NOTES_PER_BAR
 
-label_compositions = []
+target_compositions = []
 
 class MusicCloneEnv(MusicEnv):
     """
     Music environment that attempts to clone an existing melody.
     """
     def _step(self, action):
-        state, reward, done, info = super()._step(action)
+        reward = 0
+        # The target we want our action to match.
+        target = self.target_composition[self.beat + 1]
 
         # Award for action matching example composition
-        if action == self.label_composition[self.beat - 1]:
+        if action == target:
             reward += self.reward_amount
 
-        if self.beat == len(self.label_composition):
-            done = True
-
-        return state, reward, done, info
+        state = self._current_state()
+        done = self.beat == len(self.target_composition) - 2
+        self.beat += 1
+        return state, reward, done, {}
 
     def _reset(self):
         # TODO: Avoid globals
         # A global list of compositions used as example for training
-        global label_compositions
-        self.label_composition = random.choice(label_compositions)
-        self.num_notes = len(self.label_composition)
-        self.reward_amount = 1. / self.num_notes
-
-        self.composition = [self.label_composition[0]]
+        global target_compositions
+        self.target_composition = random.choice(target_compositions)
+        self.num_notes = len(self.target_composition)
+        # Every correct prediction gets 1 reward
+        self.reward_amount = 1. / (self.num_notes - 1)
         self.beat = 0
         return self._current_state()
+
+    def _current_state(self):
+        return (self.target_composition[self.beat], self.beat % NOTES_PER_BAR)
--- music/util.py
+++ music/util.py
@@ -18,7 +18,7 @@ NOTE_OFF = 0
 NO_EVENT = 1
 
 # Number of octaves supported
-NUM_OCTAVES = 3
+NUM_OCTAVES = 4
 
 # Min note in MIDI supported
 MIN_NOTE = 36
